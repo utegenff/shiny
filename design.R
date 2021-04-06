5734521ae6af9e9aecea3473fcf8c163aad8ba1b
utegenff <66421601+utegenff@users.noreply.github.com>
2021-04-06 17:40:52 +0200

    Update design.R

--- design.R
+++ design.R
@@ -1,24 +1,20 @@
-library(shiny)
-
-shinyUI(pageWithSidebar(
-  headerPanel("CSV Viewer"),
-  sidebarPanel(
-    fileInput('file1', 'dataset.csv',
-              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-    tags$hr(),
-    checkboxInput('header', 'Header', TRUE),
-    radioButtons('sep', 'Separator',
-                 c(Comma=',',
-                   Semicolon=';',
-                   Tab='\t'),
-                 'Comma'),
-    radioButtons('quote', 'Quote',
-                 c(None='',
-                   'Double Quote'='"',
-                   'Single Quote'="'"),
-                 'Double Quote')
-  ),
-  mainPanel(
-    tableOutput('contents')
-  )
-))
+ui <- navbarPage("SuicideRate", theme = shinytheme("flatly"),
+  tabPanel("Main",
+    titlePanel(div(windowTitle = "GraduatEmploymentSG", img(src = "sg0.jpg", width = "100%", class = "bg"),)),
+    
+    Panel: Main>Summary             
+    ##########################################
+    
+    tabsetPanel(type = "tabs",
+      
+      tabPanel("Summary",        
+        sidebarLayout(
+          sidebarPanel(h3("Data by Year"),
+            selectInput("checkYear", "Select Year", choices = list("2018", "2017", "2016", "2015", "2014", "2013"),selected = "2018")),
+          
+          mainPanel(
+            tabsetPanel(type = "tabs",
+              tabPanel("Ranking", tableOutput("datahead")),
+              tabPanel("No. of Graduates", plotOutput(outputId = "piePlot"))),)),
+  
+      tabPanel()))
