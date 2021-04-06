1acff643fc48a1f9e4dd9968f2efd9917795b593
utegenff <66421601+utegenff@users.noreply.github.com>
2021-04-06 14:24:43 +0200

    Update design.R

--- design.R
+++ design.R
@@ -6,7 +6,16 @@ shinyUI(pageWithSidebar(
   # Application title
   headerPanel("Miles Per Gallon"),
 
-  sidebarPanel(),
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("variable", "Variable:",
+                list("Cylinders" = "cyl", 
+                     "Transmission" = "am", 
+                     "Gears" = "gear")),
+
+    checkboxInput("outliers", "Show outliers", FALSE)
+  ),
 
   mainPanel()
 ))
