b297d3c319277cb6a6abe1dc403b3f90a58cc11e
utegenff <66421601+utegenff@users.noreply.github.com>
2021-04-06 14:58:16 +0200

    Update design.R

--- design.R
+++ design.R
@@ -1,21 +1,24 @@
 library(shiny)
 
-# Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
-
-  # Application title
-  headerPanel("Miles Per Gallon"),
-
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
+  headerPanel("CSV Viewer"),
   sidebarPanel(
-    selectInput("variable", "Variable:",
-                list("Cylinders" = "cyl", 
-                     "Transmission" = "am", 
-                     "Gears" = "gear")),
-
-    checkboxInput("outliers", "Show outliers", FALSE)
+    fileInput('file1', 'Choose CSV File',
+              accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+    tags$hr(),
+    checkboxInput('header', 'Header', TRUE),
+    radioButtons('sep', 'Separator',
+                 c(Comma=',',
+                   Semicolon=';',
+                   Tab='\t'),
+                 'Comma'),
+    radioButtons('quote', 'Quote',
+                 c(None='',
+                   'Double Quote'='"',
+                   'Single Quote'="'"),
+                 'Double Quote')
   ),
-
-  mainPanel()
+  mainPanel(
+    tableOutput('contents')
+  )
 ))
